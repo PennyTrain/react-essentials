16b6365f0d427e1060296493b039b2320088090f
Penny Train <penny.train04@gmail.com>
2024-03-22 16:10:46 +0000

    useEffect hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,11 +20,13 @@ import SearchBar from './components/SearchBar';
 import ControlledFormHooks from './components/ControlledFormHooks';
 import UseStateWithArrays from './components/UseStateWithArrays';
 import UseStateWithObjects from './components/UseStateWithObjects';
+import UseEffectCounter from './components/UseEffectCounter';
+import UseEffectCounterContainer from './components/UseEffectCounterContainer';
 
 function App() {
   return (
     <div className="App">
-      <UseStateWithObjects />
+      <UseEffectCounterContainer />
     </div>
   );
 }
